f64c9e9e8df5e1e029a82def8d77219402be53d3
466032409 <112259725+466032409@users.noreply.github.com>
2022-08-27 22:03:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const config = {
+xport const config = {
     // 公众号配置
     // 公众号appId
     appId: "wx443e85f3a4ce0891",
@@ -14,11 +14,19 @@ export const config = {
     province: "河南",
     // 所在城市
     city: "新乡",
+    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
+    birthdays: [
+      {"name": "宝宝", "year": "2001", "date": "04-27", "type": 'new'},
+    ],
+
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
+// 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
+// {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
