1085ca2c006dff9a1fe59f9c73478a5cb6f0d8d7
466032409 <112259725+466032409@users.noreply.github.com>
2022-08-27 21:33:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,30 +1,19 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx443e85f3a4ce0891",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "812692fcbfae0742eb6252a43d53abc1",
     // 模板消息id
-    templateId: "",
+    templateId: "PHvvtDZCQPZyl0bEoVz2KUe5M7X2yOjhHduxf8AOnqs",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [onUQj53rotpTxHIQ1f8uSdxEyw8s],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "河南",
     // 所在城市
-    city: "惠州",
-    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
-    ],
-    // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
-    }
-
+    city: "新乡",
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
 // 天气：{{weather.DATA}}  
